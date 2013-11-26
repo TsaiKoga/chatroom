721442497e04d45f6ce070ea681f5fbccea20823
TsaiKoga <969024089@qq.com>
2013-11-26 08:29:25 +0800

    Modify engine loaded

--- lib/chatroom.rb
+++ lib/chatroom.rb
@@ -1,6 +1,6 @@
 module Chatroom
 
-	require "chatroom/engine", if defined?(Rails)
+	require 'chatroom/engine' if defined?(Rails)
 
 	# Default way to setup Chatroom. Run rails generate chatroom_install to create
 	# a fresh initializer with all configuration values.
